0c450edfe4edec7753ee1a427e0b433e0a5f2507
mailtoravi437 <68932649+mailtoravi437@users.noreply.github.com>
2022-10-19 21:23:23 +0530

    Time: 675 ms (5.01%), Space: 157.2 MB (5.09%) - LeetHub

--- 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.java
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.java
@@ -2,31 +2,28 @@ class Solution {
     public int maxProfit(int[] prices) {
         int n = prices.length;
         Integer dp[][][] = new Integer[n+2][4][4];
-        
         return sol(0,0,2,prices,dp);
     }
     
-    public int sol(int idx,int buy,int k,int prices[],Integer dp[][][]){
+    public int sol(int buy,int idx,int k,int prices[],Integer dp[][][]){
         if(idx>=prices.length || k==0){
             return 0;
         }
-        
         
         if(dp[idx][buy][k]!=null){
             return dp[idx][buy][k];
         }
         
-        int maxProfit = 0;
-        
+        int maxProfit = Integer.MIN_VALUE;
         if(buy==0){
-            int buyToday = -prices[idx]+sol(idx+1,1,k,prices,dp);
-            int notBuy = sol(idx+1,0,k,prices,dp);
+            int buyToday = -prices[idx]+sol(1,idx+1,k,prices,dp);
+            int notBuy = sol(0,idx+1,k,prices,dp);
             
             maxProfit = Math.max(buyToday,notBuy);
         }
         else{
-            int sellToday = prices[idx]+sol(idx+1,0,k-1,prices,dp);
-            int notSell = sol(idx+1,1,k,prices,dp);
+            int sellToday = prices[idx]+sol(0,idx+1,k-1,prices,dp);
+            int notSell = sol(1,idx+1,k,prices,dp);
             
             maxProfit = Math.max(sellToday,notSell);
         }
