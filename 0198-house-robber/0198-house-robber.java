e3bc061df5d9c0dd9c6c733d8854b4276c850c86
mailtoravi437 <68932649+mailtoravi437@users.noreply.github.com>
2022-10-19 22:39:41 +0530

    Time: 0 ms (100.00%), Space: 39.4 MB (96.86%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -1,12 +1,10 @@
 class Solution {
     public int rob(int[] nums) {
         int n = nums.length;
-        
         if(n==1){
             return nums[0];
         }
-        
-        if(n==2){
+        else if(n==2){
             return Math.max(nums[0],nums[1]);
         }
         
@@ -15,8 +13,9 @@ class Solution {
         dp[1] = Math.max(nums[0],nums[1]);
         
         for(int i=2;i<n;i++){
-            int in = nums[i]+dp[i-2];
+            int in = dp[i-2] + nums[i];
             int ex = dp[i-1];
+            
             
             dp[i] = Math.max(in,ex);
         }
