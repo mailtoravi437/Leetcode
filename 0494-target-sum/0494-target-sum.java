f816c88dbd219d5df9b1d7741d23fae9ea7d3df5
mailtoravi437 <68932649+mailtoravi437@users.noreply.github.com>
2022-10-20 10:42:13 +0530

    Time: 26 ms (62.22%), Space: 53.7 MB (19.20%) - LeetHub

--- 0494-target-sum/0494-target-sum.java
+++ 0494-target-sum/0494-target-sum.java
@@ -1,10 +1,11 @@
 class Solution {
     public int findTargetSumWays(int[] nums, int target) {
         int count = 0;
-        return sol(0,0,target,nums,count);
+        Integer dp[][] = new Integer[nums.length+2][2001];
+        return sol(0,0,target,nums,count,dp);
     }
     
-    public int sol(int idx,int sum,int target,int nums[],int count){
+    public int sol(int idx,int sum,int target,int nums[],int count,Integer dp[][]){
         if(idx==nums.length){
             if(sum==target){
                 count++;
@@ -13,11 +14,16 @@ class Solution {
 
         }
         
+        if(dp[idx][sum+1001]!=null){
+            return  dp[idx][sum+1001];
+        }
         
         
-        int pos = sol(idx+1,sum+nums[idx],target,nums,count);
-        int neg = sol(idx+1,sum-nums[idx],target,nums,count);
         
-        return pos+neg;
+        int pos = sol(idx+1,sum+nums[idx],target,nums,count,dp);
+        int neg = sol(idx+1,sum-nums[idx],target,nums,count,dp);
+        
+        dp[idx][sum+1001] = pos+neg;
+        return dp[idx][sum+1001];
     }
 }
