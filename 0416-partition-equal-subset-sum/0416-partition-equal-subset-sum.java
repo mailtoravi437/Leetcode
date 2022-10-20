38326f4b8cbb0e40a960c52df62caa7672c43be4
mailtoravi437 <68932649+mailtoravi437@users.noreply.github.com>
2022-10-20 09:41:36 +0530

    Time: 89 ms (47.69%), Space: 72.4 MB (21.50%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
@@ -8,18 +8,15 @@ class Solution {
         if(sum%2!=0){
             return false;
         }
-        
         sum = sum/2;
-        
-        Boolean dp[][] = new Boolean[nums.length+1][sum+1];
-        return sol(0,nums,sum,dp);
+        Boolean dp[][] = new Boolean[nums.length+2][sum+2];
+        return sol(0,sum,nums,dp);
     }
     
-    public boolean sol(int idx,int nums[],int sum,Boolean dp[][]){
+    public boolean sol(int idx,int sum,int nums[],Boolean dp[][]){
         if(sum==0){
             return true;
         }
-        
         if(idx>=nums.length || sum<0){
             return false;
         }
@@ -28,7 +25,8 @@ class Solution {
             return dp[idx][sum];
         }
         
-        dp[idx][sum] = sol(idx+1,nums,sum-nums[idx],dp) || sol(idx+1,nums,sum,dp);
+        dp[idx][sum] = sol(idx+1,sum-nums[idx],nums,dp) || sol(idx+1,sum,nums,dp);
+        
         return dp[idx][sum];
     }
 }
