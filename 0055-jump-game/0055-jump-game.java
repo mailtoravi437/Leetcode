b84e13096a4695cefa2f55e2787e6a1d9b1d1ade
mailtoravi437 <68932649+mailtoravi437@users.noreply.github.com>
2022-10-21 12:42:17 +0530

    Time: 4 ms (63.43%), Space: 67.9 MB (53.88%) - LeetHub

--- 0055-jump-game/0055-jump-game.java
+++ 0055-jump-game/0055-jump-game.java
@@ -2,11 +2,11 @@ class Solution {
     public boolean canJump(int[] nums) {
         int n = nums.length;
         int goal = n-1;
+        
         for(int i=n-1;i>=0;i--){
             if(nums[i]+i>=goal){
                 goal = i;
             }
-            
         }
         
         return goal==0?true:false;
