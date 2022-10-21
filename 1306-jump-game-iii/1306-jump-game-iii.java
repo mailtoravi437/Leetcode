8481b050d8e319975eaaf4e8a6079e7a2a64c4ba
mailtoravi437 <68932649+mailtoravi437@users.noreply.github.com>
2022-10-21 22:27:39 +0530

    Time: 25 ms (15.27%), Space: 64.9 MB (13.55%) - LeetHub

--- 1306-jump-game-iii/1306-jump-game-iii.java
+++ 1306-jump-game-iii/1306-jump-game-iii.java
@@ -1,5 +1,31 @@
 class Solution {
     public boolean canReach(int[] arr, int start) {
-        return 0<=start && start<arr.length && arr[start]>=0 && ((arr[start]=-arr[start])==0 || canReach(arr,arr[start]+start) || canReach(arr,start-arr[start]));
+        int n = arr.length;
+        HashSet<Integer> hset = new HashSet<>();
+        Queue<Integer> queue = new LinkedList<>();
+        queue.add(start);
+        
+        while(!queue.isEmpty()){
+            int i = queue.poll();
+            if(arr[i]==0){
+                return true;
+            }
+            
+            if(hset.contains(i)){
+                continue;
+            }
+            
+            hset.add(i);
+            
+            if(i+arr[i]<n){
+                queue.add(i+arr[i]);
+            }
+            
+            if(i-arr[i]>=0){
+                queue.add(i-arr[i]);
+            }
+        }
+        
+        return false;
     }
 }
