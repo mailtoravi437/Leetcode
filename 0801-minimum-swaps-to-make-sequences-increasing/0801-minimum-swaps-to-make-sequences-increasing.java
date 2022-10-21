5476e9bec774b996f3e8bd93feae8d38bde5d9c3
mailtoravi437 <68932649+mailtoravi437@users.noreply.github.com>
2022-10-21 10:25:34 +0530

    Time: 17 ms (65.17%), Space: 89.6 MB (70.87%) - LeetHub

--- 0801-minimum-swaps-to-make-sequences-increasing/0801-minimum-swaps-to-make-sequences-increasing.java
+++ 0801-minimum-swaps-to-make-sequences-increasing/0801-minimum-swaps-to-make-sequences-increasing.java
@@ -3,21 +3,23 @@ class Solution {
         int n = nums1.length;
         
         int swap[] = new int[n];
-        int notswap[] = new int[n];
+        int nonswap[] = new int[n];
         
         swap[0] = 1;
+        
         for(int i=1;i<n;i++){
-            notswap[i] = swap[i]  = n;
+            swap[i] = nonswap[i] = n;
             if(nums1[i-1]<nums1[i] && nums2[i-1]<nums2[i]){
                 swap[i] = swap[i-1]+1;
-                notswap[i] = notswap[i-1];
+                nonswap[i] = nonswap[i-1];
             }
+            
             if(nums1[i-1]<nums2[i] && nums2[i-1]<nums1[i]){
-                swap[i] = Math.min(notswap[i-1]+1,swap[i]);
-                notswap[i] = Math.min(notswap[i],swap[i-1]);
+                swap[i] = Math.min(swap[i],nonswap[i-1]+1);
+                nonswap[i] = Math.min(swap[i-1],nonswap[i]);
             }
         }
         
-        return Math.min(swap[n-1],notswap[n-1]);
+        return Math.min(swap[n-1],nonswap[n-1]);
     }
 }
