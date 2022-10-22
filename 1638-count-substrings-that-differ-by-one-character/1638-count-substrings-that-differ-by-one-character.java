28e2074a82d8efc885fcaba69538154a9ef28a10
mailtoravi437 <68932649+mailtoravi437@users.noreply.github.com>
2022-10-22 15:41:13 +0530

    Time: 46 ms (22.13%), Space: 44.1 MB (26.48%) - LeetHub

--- 1638-count-substrings-that-differ-by-one-character/1638-count-substrings-that-differ-by-one-character.java
+++ 1638-count-substrings-that-differ-by-one-character/1638-count-substrings-that-differ-by-one-character.java
@@ -5,11 +5,10 @@ class Solution {
         
         int dp[][][] = new int[n+1][m+1][2];
         int res = 0;
-        
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
-                dp[i+1][j+1][0] = (s.charAt(i)==t.charAt(j)) ? dp[i][j][0]+1:0;
-                dp[i+1][j+1][1] = (s.charAt(i)==t.charAt(j)) ? dp[i][j][1] : dp[i][j][0]+1;
+                dp[i+1][j+1][0] = (s.charAt(i)==t.charAt(j))?dp[i][j][0]+1:0;
+                dp[i+1][j+1][1] = (s.charAt(i)==t.charAt(j))?dp[i][j][1]:dp[i][j][0]+1;
                 res += dp[i+1][j+1][1];
             }
         }
