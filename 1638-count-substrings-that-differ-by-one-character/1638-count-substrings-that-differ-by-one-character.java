fc52fe05ad3d6f294132d5453fd11b0b2a21a8b2
mailtoravi437 <68932649+mailtoravi437@users.noreply.github.com>
2022-10-22 15:29:01 +0530

    Time: 51 ms (20.95%), Space: 44.4 MB (25.69%) - LeetHub

--- 1638-count-substrings-that-differ-by-one-character/1638-count-substrings-that-differ-by-one-character.java
+++ 1638-count-substrings-that-differ-by-one-character/1638-count-substrings-that-differ-by-one-character.java
@@ -1,18 +1,17 @@
 class Solution {
     public int countSubstrings(String s, String t) {
-        int m = s.length();
-        int n = t.length();
+        int n = s.length();
+        int m = t.length();
+        
+        int dp[][][] = new int[n+1][m+1][2];
         int res = 0;
         
-        int dp[][][] = new int[m+1][n+1][2];
-        
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                dp[i+1][j+1][0] = s.charAt(i)==t.charAt(j)? dp[i][j][0]+1:0;
-                dp[i+1][j+1][1] = s.charAt(i)==t.charAt(j)?dp[i][j][1] :dp[i][j][0]+1;
-                
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                dp[i+1][j+1][0] = (s.charAt(i)==t.charAt(j)) ? dp[i][j][0]+1:0;
+                dp[i+1][j+1][1] = (s.charAt(i)==t.charAt(j)) ? dp[i][j][1] : dp[i][j][0]+1;
                 res += dp[i+1][j+1][1];
-             }
+            }
         }
         
         return res;
