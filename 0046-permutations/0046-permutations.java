8b0090e72916fc2c496c438a444f99bd38a9e78e
mailtoravi437 <68932649+mailtoravi437@users.noreply.github.com>
2022-10-24 11:01:07 +0530

    Time: 4 ms (29.94%), Space: 44.1 MB (81.03%) - LeetHub

---
+++ 0046-permutations/0046-permutations.java
@@ -0,0 +1,23 @@
+class Solution {
+    public List<List<Integer>> permute(int[] nums) {
+        List<List<Integer>> ans = new ArrayList<>();
+        List<Integer> res = new ArrayList<>();
+        sol(nums,ans,res);
+        return ans;
+    }
+    
+    public void sol(int nums[],List<List<Integer>> ans,List<Integer> res){
+        if(res.size()==nums.length){
+            ans.add(new ArrayList<>(res));
+        }
+        
+        for(int i=0;i<nums.length;i++){
+            if(res.contains(nums[i])){
+                continue;
+            }
+            res.add(nums[i]);
+            sol(nums,ans,res);
+            res.remove(res.size()-1);
+        }
+    }
+}
