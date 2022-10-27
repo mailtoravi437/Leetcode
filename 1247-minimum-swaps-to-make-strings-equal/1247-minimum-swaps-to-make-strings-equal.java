29cb40b08fe309a65760b3b3bdcdbe078f6d1ca5
mailtoravi437 <68932649+mailtoravi437@users.noreply.github.com>
2022-10-27 13:15:25 +0530

    Time: 3 ms (10.78%), Space: 42.4 MB (13.77%) - LeetHub

---
+++ 1247-minimum-swaps-to-make-strings-equal/1247-minimum-swaps-to-make-strings-equal.java
@@ -0,0 +1,36 @@
+class Solution {
+    public int minimumSwap(String s1, String s2) {
+        int x1 = 0;
+        int x2 = 0;
+        int y1 = 0;
+        int y2 = 0;
+        
+        for(int i=0;i<s1.length();i++){
+            if(s1.charAt(i)==s2.charAt(i)){
+                continue;
+            }
+            
+            if(s1.charAt(i)=='x'){
+                x1++;
+            }
+            else{
+                y1++;
+            }
+            
+            if(s2.charAt(i)=='x'){
+                x2++;
+            }
+            else{
+                y2++;
+            }
+        }
+        
+        if((x1+x2)%2!=0 || (y1+y2)%2!=0){
+            return -1;
+        }
+        
+        int ans = 0;
+        
+        return (x1/2)+(y1/2)+(x1%2)*2;
+    }
+}
