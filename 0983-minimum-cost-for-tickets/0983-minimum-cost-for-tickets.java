db22aecfeb777f87f603a315171600788664f4fc
mailtoravi437 <68932649+mailtoravi437@users.noreply.github.com>
2022-10-28 18:26:35 +0530

    Time: 1 ms (97.04%), Space: 41.4 MB (84.12%) - LeetHub

--- 0983-minimum-cost-for-tickets/0983-minimum-cost-for-tickets.java
+++ 0983-minimum-cost-for-tickets/0983-minimum-cost-for-tickets.java
@@ -1,10 +1,11 @@
 class Solution {
     public int mincostTickets(int[] days, int[] costs) {
-        Integer dp[] = new Integer[days.length+2];
+        int n = days.length;
+        Integer dp[] = new Integer[n+2];
         return sol(days,costs,dp,0);
-    }
+    } 
     
-    public int sol(int days[],int costs[],Integer dp[],int idx){
+    public int  sol(int days[],int costs[],Integer dp[],int idx){
         if(idx>=days.length){
             return 0;
         }
@@ -14,7 +15,6 @@ class Solution {
         }
         
         int oneDay = costs[0]+sol(days,costs,dp,idx+1);
-        
         int i;
         for(i=idx;i<days.length;i++){
             if(days[i]>=days[idx]+7){
@@ -23,7 +23,6 @@ class Solution {
         }
         
         int sevenDay = costs[1]+sol(days,costs,dp,i);
-        
         for(i=idx;i<days.length;i++){
             if(days[i]>=days[idx]+30){
                 break;
