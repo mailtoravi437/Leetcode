3f45b167148fe07ce76df4f839daba297beac2ce
mailtoravi437 <68932649+mailtoravi437@users.noreply.github.com>
2022-10-29 18:31:52 +0530

    Time: 0 ms (100.00%), Space: 39.3 MB (99.18%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.java
+++ 0213-house-robber-ii/0213-house-robber-ii.java
@@ -3,24 +3,23 @@ class Solution {
         if(nums.length==1){
             return nums[0];
         }
-        int res1 = sol(nums,0,nums.length-2);
-        int res2 = sol(nums,1,nums.length-1);
         
-        return Math.max(res1,res2);
+        return Math.max(sol(nums,0,nums.length-2),sol(nums,1,nums.length-1));
     }
     
     public int sol(int nums[],int s,int e){
         int include = 0;
         int exclude = 0;
+        
         for(int i=s;i<=e;i++){
-            int temp = exclude;
-            if(nums[i]+include>exclude){
-                exclude = nums[i]+include;
-            }
+            int in = include;
+            int ex = exclude;
             
-            include = temp;
+            include = ex+nums[i];
+            
+            exclude = Math.max(in,ex);
         }
         
-        return exclude;
+        return Math.max(include,exclude);
     }
 }
