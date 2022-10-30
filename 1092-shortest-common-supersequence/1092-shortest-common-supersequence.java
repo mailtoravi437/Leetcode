d1d74faec2e08d757b3168479fc9927817bf497c
mailtoravi437 <68932649+mailtoravi437@users.noreply.github.com>
2022-10-30 18:18:51 +0530

    Time: 22 ms (79.58%), Space: 46.9 MB (54.18%) - LeetHub

--- 1092-shortest-common-supersequence/1092-shortest-common-supersequence.java
+++ 1092-shortest-common-supersequence/1092-shortest-common-supersequence.java
@@ -3,16 +3,9 @@ class Solution {
         int m = str1.length();
         int n = str2.length();
         
-        String ans = sol(str1,m,str2,n);
-        return ans;
-    }
-    
-    public String sol(String str1,int m,String str2,int n){
         int dp[][] = new int[m+1][n+1];
-        
         for(int i=1;i<=m;i++){
             for(int j=1;j<=n;j++){
-                
                 if(str1.charAt(i-1)==str2.charAt(j-1)){
                     dp[i][j] = 1+dp[i-1][j-1];
                 }
@@ -23,33 +16,30 @@ class Solution {
         }
         
         StringBuilder sb = new StringBuilder();
-        int i = m;
-        int j = n;
-        
-        while(i>0 && j>0){
-            if(str1.charAt(i-1)==str2.charAt(j-1)){
-                sb.append(str1.charAt(i-1));
-                i--;
-                j--;
+        while(m>0 && n>0){
+            if(str1.charAt(m-1)==str2.charAt(n-1)){
+                sb.append(str1.charAt(m-1));
+                m--;
+                n--;
             }
-            else if(dp[i-1][j]>dp[i][j-1]){
-                sb.append(str1.charAt(i-1));
-                i--;
+            else if(dp[m-1][n]>dp[m][n-1]){
+                sb.append(str1.charAt(m-1));
+                m--;
             }
             else{
-                sb.append(str2.charAt(j-1));
-                j--;
+                sb.append(str2.charAt(n-1));
+                n--;
             }
         }
         
-        while(i>0){
-            sb.append(str1.charAt(i-1));
-            i--;
+        while(m>0){
+            sb.append(str1.charAt(m-1));
+            m--;
         }
         
-        while(j>0){
-            sb.append(str2.charAt(j-1));
-            j--;
+        while(n>0){
+            sb.append(str2.charAt(n-1));
+            n--;
         }
         
         return sb.reverse().toString();
