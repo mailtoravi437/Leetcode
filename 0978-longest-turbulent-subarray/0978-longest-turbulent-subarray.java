e833923aff85eb883e7e2671f307457fc7bec1ac
mailtoravi437 <68932649+mailtoravi437@users.noreply.github.com>
2022-10-30 13:28:09 +0530

    Time: 13 ms (43.16%), Space: 67.2 MB (37.45%) - LeetHub

--- 0978-longest-turbulent-subarray/0978-longest-turbulent-subarray.java
+++ 0978-longest-turbulent-subarray/0978-longest-turbulent-subarray.java
@@ -5,7 +5,6 @@ class Solution {
         }
         
         int n = arr.length;
-        
         int inc[] = new int[n];
         int dec[] = new int[n];
         
@@ -15,10 +14,10 @@ class Solution {
         int res = 1;
         
         for(int i=1;i<n;i++){
-            if(arr[i]>arr[i-1]){
+            if(arr[i-1]<arr[i]){
                 inc[i] = dec[i-1]+1;
             }
-            else if(arr[i]<arr[i-1]){
+            else if(arr[i-1]>arr[i]){
                 dec[i] = inc[i-1]+1;
             }
             
