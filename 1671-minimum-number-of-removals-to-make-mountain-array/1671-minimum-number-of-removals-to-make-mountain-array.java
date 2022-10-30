c29f09598c694a539b3aaacdac2786235398f3f4
mailtoravi437 <68932649+mailtoravi437@users.noreply.github.com>
2022-10-31 03:58:38 +0530

    Time: 84 ms (35.40%), Space: 45.9 MB (73.16%) - LeetHub

--- 1671-minimum-number-of-removals-to-make-mountain-array/1671-minimum-number-of-removals-to-make-mountain-array.java
+++ 1671-minimum-number-of-removals-to-make-mountain-array/1671-minimum-number-of-removals-to-make-mountain-array.java
@@ -9,7 +9,7 @@ class Solution {
         
         for(int i=0;i<n;i++){
             for(int j=0;j<i;j++){
-                if(nums[i]>nums[j] && lis[j]+1>lis[i]){
+                if(nums[i]>nums[j] && lis[i]<lis[j]+1){
                     lis[i] = lis[j]+1;
                 }
             }
@@ -17,21 +17,20 @@ class Solution {
         
         for(int i=n-1;i>=0;i--){
             for(int j=n-1;j>i;j--){
-                if(nums[i]>nums[j] && dis[j]+1>dis[i]){
+                if(nums[i]>nums[j] && dis[i]<dis[j]+1){
                     dis[i] = dis[j]+1;
                 }
             }
         }
         
-        int max = 0;
+        int res = 0;
         for(int i=0;i<n;i++){
             if(lis[i]==1 || dis[i]==1){
                 continue;
             }
-            
-            max = Math.max(lis[i]+dis[i]-1,max);
+            res = Math.max(lis[i]+dis[i]-1,res);
         }
         
-        return n-max;
+        return n-res;
     }
 }
