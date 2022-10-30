25240434440ec7ac7088c28eab4d01f310f88b0f
mailtoravi437 <68932649+mailtoravi437@users.noreply.github.com>
2022-10-30 16:44:04 +0530

    Time: 680 ms (9.78%), Space: 323.6 MB (5.00%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
@@ -2,13 +2,14 @@ class Solution {
     public String longestPalindrome(String s) {
         int n = s.length();
         boolean dp[][] = new boolean[n][n];
-        
         String ans = "";
+        
         for(int g=0;g<n;g++){
             for(int i=0,j=g;j<n;i++,j++){
                 if(g==0){
                     dp[i][j] = true;
                 }
+                
                 else if(g==1){
                     if(s.charAt(i)==s.charAt(j)){
                         dp[i][j] = true;
@@ -25,9 +26,9 @@ class Solution {
                 if(dp[i][j]==true){
                     ans = s.substring(i,j+1);
                 }
-                
             }
         }
+        
         return ans;
     }
 }
