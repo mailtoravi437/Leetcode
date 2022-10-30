d21ab5c4173ae3f23805720c7e23af0637c54ad9
mailtoravi437 <68932649+mailtoravi437@users.noreply.github.com>
2022-10-30 18:29:48 +0530

    Time: 85 ms (13.31%), Space: 54.3 MB (48.92%) - LeetHub

--- 1312-minimum-insertion-steps-to-make-a-string-palindrome/1312-minimum-insertion-steps-to-make-a-string-palindrome.java
+++ 1312-minimum-insertion-steps-to-make-a-string-palindrome/1312-minimum-insertion-steps-to-make-a-string-palindrome.java
@@ -1,31 +1,33 @@
 class Solution {
     public int minInsertions(String s) {
         int n = s.length();
-        
         String x = new StringBuilder(s).reverse().toString();
-                int dp[][] = new int[n+2][n+2];
-
-        for(int i=0;i<=n+1;i++){
-            for(int j=0;j<=n+1;j++){
+        
+        int dp[][] = new int[n+2][n+2];
+        for(int i=0;i<dp.length;i++){
+            for(int j=0;j<dp[0].length;j++){
                 dp[i][j] = -1;
             }
         }
-        return n-lcs(s,n,x,n,dp);
+        
+        int ans =  sol(s,n,x,n,dp);
+        
+        return n-ans;
     }
     
-    public int lcs(String s,int n,String x,int m,int dp[][]){
-        if(n==0 || m==0){
+    public int sol(String s,int i,String x,int j,int dp[][]){
+        if(i==0 || j==0){
             return 0;
         }
         
-        if(dp[n][m]!=-1){
-            return dp[n][m];
+        if(dp[i][j]!=-1){
+            return dp[i][j];
         }
         
-        if(s.charAt(n-1)==x.charAt(m-1)){
-            return dp[n][m] = 1+lcs(s,n-1,x,m-1,dp);
+        if(s.charAt(i-1)==x.charAt(j-1)){
+            return dp[i][j] = 1+sol(s,i-1,x,j-1,dp);
         }
         
-        return dp[n][m] = Math.max(lcs(s,n-1,x,m,dp),lcs(s,n,x,m-1,dp));
+        return dp[i][j] = Math.max(sol(s,i-1,x,j,dp),sol(s,i,x,j-1,dp));
     }
 }
