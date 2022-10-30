b5be7ba7f1d6c0ba0397bd947823dcf2c2e04cc3
mailtoravi437 <68932649+mailtoravi437@users.noreply.github.com>
2022-10-30 13:25:03 +0530

    Time: 29 ms (12.74%), Space: 69.3 MB (7.99%) - LeetHub

---
+++ 0978-longest-turbulent-subarray/0978-longest-turbulent-subarray.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int maxTurbulenceSize(int[] arr) {
+        if(arr.length==0){
+            return 0;
+        }
+        
+        int n = arr.length;
+        
+        int inc[] = new int[n];
+        int dec[] = new int[n];
+        
+        Arrays.fill(inc,1);
+        Arrays.fill(dec,1);
+        
+        int res = 1;
+        
+        for(int i=1;i<n;i++){
+            if(arr[i]>arr[i-1]){
+                inc[i] = dec[i-1]+1;
+            }
+            else if(arr[i]<arr[i-1]){
+                dec[i] = inc[i-1]+1;
+            }
+            
+            res = Math.max(res,Math.max(inc[i],dec[i]));
+        }
+        
+        return res;
+    }
+}
