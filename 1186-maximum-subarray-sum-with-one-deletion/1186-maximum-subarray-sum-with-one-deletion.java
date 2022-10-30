ff304de69a97b2993432f69ad44238e177ae8569
mailtoravi437 <68932649+mailtoravi437@users.noreply.github.com>
2022-10-30 13:58:22 +0530

    Time: 6 ms (75.68%), Space: 59.1 MB (45.56%) - LeetHub

--- 1186-maximum-subarray-sum-with-one-deletion/1186-maximum-subarray-sum-with-one-deletion.java
+++ 1186-maximum-subarray-sum-with-one-deletion/1186-maximum-subarray-sum-with-one-deletion.java
@@ -1,25 +1,24 @@
 class Solution {
     public int maximumSum(int[] arr) {
-        int n = arr.length;
+        int n = arr.length; 
         int mel[] = new int[n];
-        int mer[] = new int[n];
+        int meh[] = new int[n];
         
         mel[0] = arr[0];
         int max = arr[0];
         
         for(int i=1;i<n;i++){
             mel[i] = Math.max(mel[i-1]+arr[i],arr[i]);
-            max = Math.max(max,mel[i]);
+            max = Math.max(mel[i],max);
         }
         
-        mer[n-1] = arr[n-1];
-        
+        meh[n-1] = arr[n-1];
         for(int i=n-2;i>=0;i--){
-            mer[i] = Math.max(mer[i+1]+arr[i],arr[i]);
+            meh[i] = Math.max(meh[i+1]+arr[i],arr[i]);
         }
         
         for(int i=1;i<n-1;i++){
-            max = Math.max(max,mer[i+1]+mel[i-1]);
+            max = Math.max(max,mel[i-1]+meh[i+1]);
         }
         
         return max;
