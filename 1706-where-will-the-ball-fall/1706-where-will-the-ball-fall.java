335b4d01078721ca4c414a4c2a7f4efa09e0413b
mailtoravi437 <68932649+mailtoravi437@users.noreply.github.com>
2022-10-31 18:24:19 +0530

    Time: 2 ms (73.71%), Space: 43.7 MB (87.23%) - LeetHub

---
+++ 1706-where-will-the-ball-fall/1706-where-will-the-ball-fall.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int[] findBall(int[][] grid) {
+        int n = grid.length;
+        int m = grid[0].length;
+        
+        int ans[] = new int[m];
+        
+        for(int j=0;j<m;j++){
+            int currPos = j;
+            int nextPos = -1;
+            
+            for(int i=0;i<n;i++){
+                nextPos = currPos+grid[i][currPos];
+                if(nextPos<0 || nextPos>=m || grid[i][currPos]!=grid[i][nextPos]){
+                    currPos = -1;
+                    break;
+                }
+                
+                currPos = nextPos;
+            }
+            
+            ans[j] = currPos;
+        }
+        
+        return ans;
+    }
+}
