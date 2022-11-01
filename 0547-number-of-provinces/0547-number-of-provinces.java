9e481aaa4abd94727f7614f17fb86a2b4886d2a3
mailtoravi437 <68932649+mailtoravi437@users.noreply.github.com>
2022-11-01 13:12:22 +0530

    Time: 2 ms (72.37%), Space: 42.6 MB (99.67%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.java
+++ 0547-number-of-provinces/0547-number-of-provinces.java
@@ -1,41 +1,46 @@
 class Solution {
     int parent[];
     public int findCircleNum(int[][] isConnected) {
-       int n = isConnected.length;
+        int n = isConnected.length;
         int m = isConnected[0].length;
-        
         parent = new int[n*m];
+        
         for(int i=0;i<(n*m);i++){
             parent[i] = i;
         }
-        int count = n;
         
+        int count = n;
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
-               if(isConnected[i][j]==1){
-                    if(union(i,j,parent)){
-                    count--;
+                if(isConnected[i][j]==1){
+                    if(isUnion(i,j,parent)){
+                        count--;
+                    }
                 }
-               }
             }
         }
+        
         
         return count;
     }
     
-    public boolean union(int x,int y,int parent[]){
-        int px = find(x,parent);
-        int py = find(y,parent);
+    public boolean isUnion(int i,int j,int parent[]){
+        int px = find(i,parent);
+        int py = find(j,parent);
         
         if(px==py){
             return false;
         }
         
-        parent[py] = px;
+        parent[py]  = px;
         return true;
     }
     
-    public int find(int x,int parent[]){
-        return (parent[x]==x?x:(parent[x]=find(parent[x],parent)));
+    public int find(int i,int parent[]){
+        if(parent[i]==i){
+            return i;
+        }
+        
+        return parent[i] = find(parent[i],parent);
     }
 }
