8a031b0fe95817b496a22b1305089cb0d6320459
mailtoravi437 <68932649+mailtoravi437@users.noreply.github.com>
2022-11-01 13:20:02 +0530

    Time: 1 ms (93.70%), Space: 44.1 MB (42.22%) - LeetHub

---
+++ 0684-redundant-connection/0684-redundant-connection.java
@@ -0,0 +1,35 @@
+class Solution {
+    public int[] findRedundantConnection(int[][] edges) {
+        int n = edges.length;
+        int parent[] = new int[edges.length+1];
+        for(int i=0;i<=n;i++){
+            parent[i] = i;
+        }
+        
+        for(int edge[] : edges){
+            int u = edge[0];
+            int v = edge[1];
+            
+            if(find(u,parent)==find(v,parent)){
+                return edge;
+            }
+            
+            else{
+                int px = find(u,parent);
+                int py = find(v,parent);
+                
+                parent[py] = px;
+            } 
+        }
+        
+        return new int[2];
+    }
+    
+    public int find(int x,int parent[]){
+        if(x==parent[x]){
+            return x;
+        }
+        
+        return parent[x] = find(parent[x],parent);
+    }
+}
