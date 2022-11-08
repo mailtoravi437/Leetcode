6a15f7542d5de3207b2e5504d1fb5975a2ed8184
mailtoravi437 <68932649+mailtoravi437@users.noreply.github.com>
2022-11-09 00:28:00 +0530

    Time: 0 ms (100.00%), Space: 41.9 MB (87.91%) - LeetHub

--- 1022-sum-of-root-to-leaf-binary-numbers/1022-sum-of-root-to-leaf-binary-numbers.java
+++ 1022-sum-of-root-to-leaf-binary-numbers/1022-sum-of-root-to-leaf-binary-numbers.java
@@ -16,19 +16,22 @@
 class Solution {
     int sum = 0;
     public int sumRootToLeaf(TreeNode root) {
-        preorder(root,0);
+        dfs(root,0);
         return sum;
     }
     
-    public void preorder(TreeNode root,int currSum){
-        if(root!=null){
-            currSum = (currSum<<1) | root.val;
-            if(root.left==null && root.right==null){
-                sum += currSum;
-            }
-            
-            preorder(root.left,currSum);
-            preorder(root.right,currSum);
+    public void dfs(TreeNode root,int currSum){
+        if(root==null){
+            return;
         }
+        
+        currSum = (currSum<<1)+root.val;
+        
+        if(root.left==null && root.right==null){
+            sum += currSum;
+        }
+        
+        dfs(root.left,currSum);
+        dfs(root.right,currSum);
     }
 }
