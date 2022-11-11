5c674505284968e7a4152ee125dfab6c5ae2b0cd
mailtoravi437 <68932649+mailtoravi437@users.noreply.github.com>
2022-11-12 01:16:36 +0530

    Time: 19 ms (41.44%), Space: 54.2 MB (40.80%) - LeetHub

---
+++ 0329-longest-increasing-path-in-a-matrix/0329-longest-increasing-path-in-a-matrix.java
@@ -0,0 +1,41 @@
+class Solution {
+    public static final int dir[][] = {{1,0},{0,1},{-1,0},{0,-1}};
+    public int longestIncreasingPath(int[][] matrix) {
+        int m = matrix.length;
+        int n = matrix[0].length;
+        
+        Integer dp[][] = new Integer[m][n];
+        
+        int ans = 1;
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                int len = dfs(matrix,i,j,m,n,dp);
+                ans = Math.max(ans,len);
+            }
+        }
+        
+        return ans;
+    }
+    
+    public int dfs(int matrix[][],int i,int j,int m,int n,Integer dp[][]){
+        if(dp[i][j]!=null){
+            return dp[i][j];
+        }
+        
+        int max = 1;
+        for(int d[] : dir){
+            int x = i+d[0];
+            int y = j+d[1];
+            
+            if(x<0 || y<0 || x>=m || y>=n || matrix[i][j]>=matrix[x][y]){
+                continue;
+            }
+            
+            int len = 1+dfs(matrix,x,y,m,n,dp);
+            max = Math.max(len,max);
+        }
+        
+        dp[i][j] = max;
+        return max;
+    }
+}
