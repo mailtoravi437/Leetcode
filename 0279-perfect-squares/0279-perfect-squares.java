8407997a308914b2e0275d70649fa4d067ff334c
mailtoravi437 <68932649+mailtoravi437@users.noreply.github.com>
2022-11-12 17:28:00 +0530

    Time: 101 ms (40.72%), Space: 43.9 MB (32.36%) - LeetHub

--- 0279-perfect-squares/0279-perfect-squares.java
+++ 0279-perfect-squares/0279-perfect-squares.java
@@ -1,25 +1,24 @@
 class Solution {
     public int numSquares(int n) {
         Integer dp[] = new Integer[n+1];
-        return helper(n,dp);
+        return sol(n,dp);
     }
     
-    public int helper(int n,Integer dp[]){
+    public int sol(int n,Integer dp[]){
         if(n<0){
             return Integer.MAX_VALUE;
-        }
-        
-        if(dp[n]!=null){
-            return dp[n];
         }
         
         if(n==0){
             return 0;
         }
         
+        if(dp[n]!=null){
+            return dp[n];
+        }
         int min = Integer.MAX_VALUE;
         for(int i=1;(i*i)<=n;i++){
-            min = Math.min(min,helper(n-(i*i),dp));
+            min = Math.min(min,sol(n-(i*i),dp));
         }
         
         dp[n] = min+1;
