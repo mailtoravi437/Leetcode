0485f178bcea4c425ca4ec8a99c52f0dabdc9348
mailtoravi437 <68932649+mailtoravi437@users.noreply.github.com>
2022-11-12 11:43:05 +0530

    Time: 2 ms (67.11%), Space: 42.2 MB (56.61%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.java
+++ 0091-decode-ways/0091-decode-ways.java
@@ -2,10 +2,10 @@ class Solution {
     public int numDecodings(String s) {
         int n = s.length();
         Integer dp[] = new Integer[n+2];
-        return sol(s,dp,0);
+        return sol(s,0,dp);
     }
     
-    public int sol(String s,Integer dp[],int idx){
+    public int sol(String s,int idx,Integer dp[]){
         if(idx==s.length()){
             return 1;
         }
@@ -18,11 +18,11 @@ class Solution {
             return dp[idx];
         }
         
-        int way1 = sol(s,dp,idx+1);
-        int way2 = 0;
         
+        int way1 = sol(s,idx+1,dp);
+        int way2 = 0;
         if(idx<s.length()-1 && Integer.parseInt(s.substring(idx,idx+2))<=26){
-            way2 = sol(s,dp,idx+2);
+            way2 = sol(s,idx+2,dp);
         }
         
         return dp[idx] = way1+way2;
