ce1ff9f0d3637d329cbcf5215bcc6033d2c83652
mailtoravi437 <68932649+mailtoravi437@users.noreply.github.com>
2022-11-12 15:38:24 +0530

    Time: 46 ms (16.87%), Space: 49.9 MB (61.49%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.java
+++ 0115-distinct-subsequences/0115-distinct-subsequences.java
@@ -2,10 +2,11 @@ class Solution {
     public int numDistinct(String s, String t) {
         int n = s.length();
         int m = t.length();
+        
         Integer dp[][] = new Integer[n+2][m+2];
         
         return sol(s,0,t,0,dp);
-    } 
+    }
     
     public int sol(String s,int n,String t,int m,Integer dp[][]){
         if(dp[n][m]!=null){
@@ -19,24 +20,26 @@ class Solution {
         else if(n==s.length() && m<t.length()){
             return 0;
         }
-        
         else if(n==s.length()-1 && m==t.length()-1){
             if(s.charAt(n)==t.charAt(m)){
                 dp[n][m] = 1;
             }
             else{
-                dp[n][m] = 0;
+                dp[n][m] =  0;
             }
         }
+        
         else{
             int ways = 0;
             if(s.charAt(n)==t.charAt(m)){
                 ways += sol(s,n+1,t,m+1,dp);
             }
+            
             ways += sol(s,n+1,t,m,dp);
             dp[n][m] = ways;
         }
         
         return dp[n][m];
+        
     }
 }
