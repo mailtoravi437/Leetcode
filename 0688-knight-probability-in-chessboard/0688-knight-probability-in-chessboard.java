f4e8c7fe790c3ce2fb61e71f62d3b2ce02d3e889
mailtoravi437 <68932649+mailtoravi437@users.noreply.github.com>
2022-11-13 19:37:56 +0530

    Time: 15 ms (55.59%), Space: 44 MB (23.21%) - LeetHub

--- 0688-knight-probability-in-chessboard/0688-knight-probability-in-chessboard.java
+++ 0688-knight-probability-in-chessboard/0688-knight-probability-in-chessboard.java
@@ -6,9 +6,9 @@ class Solution {
     }
     
     public double sol(int N,int K,int row,int col,Double dp[][][]){
-        if(row<0 || col<0 || row>=N || col>=N){
-            return 0.0;
-        }
+       if(row<0 || col<0 || row>=N || col>=N){
+           return 0.0;
+       }
         
         if(K==0){
             return 1;
@@ -17,12 +17,11 @@ class Solution {
         if(dp[row][col][K]!=null){
             return dp[row][col][K];
         }
-        double rate = 0;
-        
+        double res = 0.0;
         for(int i=0;i<8;i++){
-            rate += 0.125*sol(N,K-1,row+dir[i][0],col+dir[i][1],dp);
+            res += 0.125*sol(N,K-1,row+dir[i][0],col+dir[i][1],dp);
         }
         
-        return dp[row][col][K] = rate; 
+        return dp[row][col][K] = res;
     }
 }
