17fd400693b8886c3516c21161a937e1b2a4fe22
mailtoravi437 <68932649+mailtoravi437@users.noreply.github.com>
2022-11-13 12:44:38 +0530

    Time: 5 ms (94.88%), Space: 41.9 MB (91.02%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
@@ -1,24 +1,26 @@
 class Solution {
     public int lengthOfLIS(int[] nums) {
         int n = nums.length;
+        
         if(n==0){
             return 0;
         }
         
         int dp[] = new int[n+1];
+        
         Arrays.fill(dp,Integer.MAX_VALUE);
         dp[0] = Integer.MIN_VALUE;
         
         for(int i=0;i<n;i++){
             int idx = lower_bound(nums[i],0,n-1,dp);
-            if(dp[idx-1]<nums[i] && dp[idx]>nums[i]){
+            if(nums[i]>dp[idx-1] && nums[i]<dp[idx]){
                 dp[idx] = nums[i];
             }
         }
         
+        int ma = n;
         int ans = 0;
         
-        int ma = n;
         for(int i=n;i>=0;i--){
             if(dp[i]!=Integer.MAX_VALUE){
                 ans = i;
@@ -29,9 +31,9 @@ class Solution {
         return ans;
     }
     
-    public int lower_bound(int key,int low,int end,int dp[]){
+    public int lower_bound(int key,int low,int high,int dp[]){
         int s = low;
-        int e = end;
+        int e = high;
         
         while(s<=e){
             int mid = s+(e-s)/2;
