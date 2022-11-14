f165e109665622d2c0a37e6d3fad478c5b8c5e30
mailtoravi437 <68932649+mailtoravi437@users.noreply.github.com>
2022-11-14 16:13:21 +0530

    Time: 19 ms (57.53%), Space: 46.5 MB (95.65%) - LeetHub

--- 1262-greatest-sum-divisible-by-three/1262-greatest-sum-divisible-by-three.java
+++ 1262-greatest-sum-divisible-by-three/1262-greatest-sum-divisible-by-three.java
@@ -1,8 +1,8 @@
 class Solution {
     public int maxSumDivThree(int[] nums) {
         int dp[] = new int[3];
-        for(int a : nums){
-            for(int i : dp.clone()){
+        for(int i : nums){
+            for(int a : dp.clone()){
                 dp[(i+a)%3] = Math.max(dp[(i+a)%3],i+a);
             }
         }
