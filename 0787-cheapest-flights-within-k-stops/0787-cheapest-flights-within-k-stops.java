f3bc57c25c10633d1fbe927e2be4048dadf09b1a
mailtoravi437 <68932649+mailtoravi437@users.noreply.github.com>
2022-11-16 15:28:57 +0530

    Time: 4 ms (97.05%), Space: 43.8 MB (83.93%) - LeetHub

--- 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.java
+++ 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.java
@@ -3,25 +3,19 @@ class Solution {
         int sta[] = new int[n];
         int tmp[] = new int[n];
         
+        Arrays.fill(sta,Integer.MAX_VALUE);
+        Arrays.fill(tmp,Integer.MAX_VALUE);
         
-        for(int i=0;i<n;i++){
-            if(i!=src){
-                sta[i] = Integer.MAX_VALUE;
-            }
-            
-            tmp[i] = sta[i];
-        }
-        
+        sta[src] = 0;
         
         for(int i=0;i<=k;i++){
             for(int j=0;j<flights.length;j++){
                 int sta_src = flights[j][0];
-                int dst_src = flights[j][1];
+                int sta_dest = flights[j][1];
                 int cost = flights[j][2];
                 
-                
-                if(sta[sta_src]!=Integer.MAX_VALUE && (sta[sta_src]+cost<tmp[dst_src])){
-                    tmp[dst_src] = sta[sta_src]+cost;
+                if(sta[sta_src]!=Integer.MAX_VALUE && sta[sta_src]+cost<tmp[sta_dest]){
+                    tmp[sta_dest] = sta[sta_src]+cost;
                 }
             }
             
@@ -30,6 +24,6 @@ class Solution {
             }
         }
         
-        return sta[dst]!=Integer.MAX_VALUE?sta[dst]:-1;
+        return sta[dst]==Integer.MAX_VALUE?-1:sta[dst];
     }
 }
