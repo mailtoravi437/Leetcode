734fc5e201daf55a24d7e788b58e1998be25171e
mailtoravi437 <68932649+mailtoravi437@users.noreply.github.com>
2022-11-19 18:46:01 +0530

    Time: 5 ms (75.07%), Space: 51.4 MB (68.97%) - LeetHub

---
+++ 0135-candy/0135-candy.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int candy(int[] rating) {
+        int n = rating.length;
+        int candies[] = new int[n];
+        Arrays.fill(candies,1);
+        
+        for(int i=1;i<n;i++){
+            if(rating[i]>rating[i-1]){
+                candies[i] = candies[i-1]+1;
+            }
+        }
+        
+        for(int i=n-2;i>=0;i--){
+            if(rating[i]>rating[i+1]){
+                candies[i] = Math.max(candies[i],candies[i+1]+1);
+            }
+        }
+        
+        int sum = 0;
+        for(int x : candies){
+            sum += x;
+        }
+        
+        return sum;
+    }
+}
