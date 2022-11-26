77f688a81782fc5405dee6bfddddb4495c1ea6b8
mailtoravi437 <68932649+mailtoravi437@users.noreply.github.com>
2022-11-26 13:37:19 +0530

    Time: 1 ms (90.28%), Space: 41.6 MB (58.10%) - LeetHub

--- 0410-split-array-largest-sum/0410-split-array-largest-sum.java
+++ 0410-split-array-largest-sum/0410-split-array-largest-sum.java
@@ -4,37 +4,40 @@ class Solution {
         int low = 0;
         int high = 0;
         int min = Integer.MAX_VALUE;
+        
         for(int i=0;i<n;i++){
             low = Math.max(low,nums[i]);
             high += nums[i];
         }
         
         while(low<=high){
-            int mid = low+((high-low)>>2);
-            if(requiredNum(mid,k,nums)){
+            int mid = low+((high-low)>>1);
+            if(requiredSol(nums,mid,k)){
                 min = Math.min(min,mid);
                 high = mid-1;
             }
+            
             else{
                 low = mid+1;
             }
         }
+        
         return min;
     }
     
-    public boolean requiredNum(int mid,int k,int nums[]){
-        int chunks =0;
+    public boolean requiredSol(int nums[],int mid,int k){
         int i = 0;
-        
+        int chunks = 0;
         while(i<nums.length){
             int val = 0;
-            while(i<nums.length && nums[i]+val<=mid){
+            while(i<nums.length && val+nums[i]<=mid){
                 val += nums[i];
                 i++;
             }
-                chunks++;   
+            
+            chunks++;
         }
+        
         return chunks<=k;
-
     }
 }
