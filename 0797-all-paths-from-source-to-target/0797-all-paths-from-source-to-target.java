2b0ca14737ecae0f36ca3446ee375b3dd22564ce
mailtoravi437 <68932649+mailtoravi437@users.noreply.github.com>
2022-11-28 02:30:34 +0530

    Time: 4 ms (78.83%), Space: 55.1 MB (45.55%) - LeetHub

--- 0797-all-paths-from-source-to-target/0797-all-paths-from-source-to-target.java
+++ 0797-all-paths-from-source-to-target/0797-all-paths-from-source-to-target.java
@@ -1,23 +1,23 @@
 class Solution {
     public List<List<Integer>> allPathsSourceTarget(int[][] graph) {
-        List<List<Integer>> ans = new ArrayList<>();
-        List<Integer> res = new ArrayList<>();
+        List<List<Integer>> res = new ArrayList<>();
+        List<Integer> ans = new ArrayList<>();
         
-        res.add(0);
-        sol(0,ans,res,graph);
-        return ans;
+        ans.add(0);
+        sol(0,res,ans,graph);
+        return res;
     }
     
-    public void sol(int idx,List<List<Integer>> ans,List<Integer> res,int graph[][]){
+    public void sol(int idx,List<List<Integer>> res,List<Integer> ans,int graph[][]){
         if(idx==graph.length-1){
-            ans.add(new ArrayList<>(res));
+            res.add(new ArrayList<>(ans));
             return;
         }
         
         for(int x : graph[idx]){
-            res.add(x);
-            sol(x,ans,res,graph);
-            res.remove(res.size()-1);
+            ans.add(x);
+            sol(x,res,ans,graph);
+            ans.remove(ans.size()-1);
         }
     }
 }
