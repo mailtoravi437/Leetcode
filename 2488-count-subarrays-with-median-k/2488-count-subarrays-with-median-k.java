e9b00b04d4f178a0dbc285f54519a45649085194
mailtoravi437 <68932649+mailtoravi437@users.noreply.github.com>
2022-11-30 00:26:55 +0530

    Time: 22 ms (82.54%), Space: 73.3 MB (21.25%) - LeetHub

--- 2488-count-subarrays-with-median-k/2488-count-subarrays-with-median-k.java
+++ 2488-count-subarrays-with-median-k/2488-count-subarrays-with-median-k.java
@@ -1,41 +1,38 @@
 class Solution {
     public int countSubarrays(int[] nums, int k) {
-        int n = nums.length;
+        int sum = 0;
         int index = -1;
-        
+        int n = nums.length;
         for(int i=0;i<n;i++){
-            if(nums[i]==k){
-                nums[i] = 0;
-                index = i;
-            }
-            else if(nums[i]<k){
+            if(nums[i]<k){
                 nums[i] = -1;
             }
-            else{
+            else if(nums[i]>k){
                 nums[i] = 1;
+            }
+            else{
+                nums[i] = 0;
+                index = i;
             }
         }
         
         Map<Integer,Integer> hmap = new HashMap<>();
-        int sum = 0;
-        
         for(int i=index;i>=0;i--){
             sum += nums[i];
             hmap.put(sum,hmap.getOrDefault(sum,0)+1);
         }
         
-        sum=0;
+        sum = 0;
         int ans = 0;
         for(int i=index;i<n;i++){
             sum += nums[i];
             int f = -1*sum;
             
             if(hmap.containsKey(f)){
-                ans += hmap.get(f);
+               ans += hmap.get(f); 
             }
             
             f += 1;
-            
             if(hmap.containsKey(f)){
                 ans += hmap.get(f);
             }
