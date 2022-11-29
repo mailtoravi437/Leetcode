b95f96d49712ccc4109b5ec4d2a910ad4a41b4bd
mailtoravi437 <68932649+mailtoravi437@users.noreply.github.com>
2022-11-30 00:04:09 +0530

    Time: 41 ms (59.58%), Space: 72.8 MB (24.29%) - LeetHub

---
+++ 2488-count-subarrays-with-median-k/2488-count-subarrays-with-median-k.java
@@ -0,0 +1,46 @@
+class Solution {
+    public int countSubarrays(int[] nums, int k) {
+        int n = nums.length;
+        int index = -1;
+        
+        for(int i=0;i<n;i++){
+            if(nums[i]==k){
+                nums[i] = 0;
+                index = i;
+            }
+            else if(nums[i]<k){
+                nums[i] = -1;
+            }
+            else{
+                nums[i] = 1;
+            }
+        }
+        
+        Map<Integer,Integer> hmap = new HashMap<>();
+        int sum = 0;
+        
+        for(int i=index;i>=0;i--){
+            sum += nums[i];
+            hmap.put(sum,hmap.getOrDefault(sum,0)+1);
+        }
+        
+        sum=0;
+        int ans = 0;
+        for(int i=index;i<n;i++){
+            sum += nums[i];
+            int f = -1*sum;
+            
+            if(hmap.containsKey(f)){
+                ans += hmap.get(f);
+            }
+            
+            f += 1;
+            
+            if(hmap.containsKey(f)){
+                ans += hmap.get(f);
+            }
+        }
+        
+        return ans;
+    }
+}
