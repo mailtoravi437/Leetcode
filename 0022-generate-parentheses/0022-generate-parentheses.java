ff1584d9b731247f3a7527ded4b0381db8f7e252
mailtoravi437 <68932649+mailtoravi437@users.noreply.github.com>
2022-12-03 03:15:02 +0530

    Time: 3 ms (55.14%), Space: 44.1 MB (24.97%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.java
+++ 0022-generate-parentheses/0022-generate-parentheses.java
@@ -5,17 +5,19 @@ class Solution {
         return res;
     }
     
-    public void sol(List<String> res,int open,int close,String str,int max){
-        if(str.length()==2*max){
-            res.add(str);
+    public void sol(List<String> res,int open,int close,String curr,int n){
+        if(curr.length()==2*n){
+            res.add(curr);
             return;
         }
         
-        if(open<max){
-            sol(res,open+1,close,str+"(",max);
+        if(open<n){
+            sol(res,open+1,close,curr+"(",n);
         }
+        
         if(close<open){
-            sol(res,open,close+1,str+")",max);
+            sol(res,open,close+1,curr+")",n);
         }
+        
     }
 }
