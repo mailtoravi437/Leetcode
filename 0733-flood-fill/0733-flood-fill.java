a97918854e00905427cba439eb249001d1cb890b
mailtoravi437 <68932649+mailtoravi437@users.noreply.github.com>
2022-12-07 01:16:34 +0530

    Time: 1 ms (87.99%), Space: 48 MB (55.42%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.java
+++ 0733-flood-fill/0733-flood-fill.java
@@ -1,23 +1,27 @@
 class Solution {
     public int[][] floodFill(int[][] image, int sr, int sc, int color) {
+        int n = image.length;
+        int m = image[0].length;
+        
         if(image[sr][sc]==color){
             return image;
         }
         
-        fill(image,sr,sc,color,image[sr][sc]);
+        int oldColor = image[sr][sc];
         
+        sol(image,sr,sc,color,oldColor);
         return image;
     }
     
-    public void fill(int image[][],int sr,int sc,int color,int oldColor){
+    public void sol(int image[][],int sr,int sc,int color,int oldColor){
         if(sr<0 || sc<0 || sr>=image.length || sc>=image[0].length || image[sr][sc]!=oldColor){
             return;
         }
         
         image[sr][sc] = color;
-        fill(image,sr+1,sc,color,oldColor);
-        fill(image,sr-1,sc,color,oldColor);
-        fill(image,sr,sc-1,color,oldColor);
-        fill(image,sr,sc+1,color,oldColor);
+        sol(image,sr+1,sc,color,oldColor);
+        sol(image,sr-1,sc,color,oldColor);
+        sol(image,sr,sc+1,color,oldColor);
+        sol(image,sr,sc-1,color,oldColor);
     }
 }
