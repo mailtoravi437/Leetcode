0f4ba384fd2659f102cc7f7c998f0a8924789267
mailtoravi437 <68932649+mailtoravi437@users.noreply.github.com>
2022-12-11 03:11:27 +0530

    Time: 713 ms (5.05%), Space: 169 MB (5.05%) - LeetHub

--- 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.java
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.java
@@ -6,28 +6,26 @@ class Solution {
     }
     
     public int sol(int prices[],int idx,int buy,int k,Integer dp[][][]){
-        if(idx>=prices.length || k==0){
-            return 0;
-        }
-        
         if(dp[idx][buy][k]!=null){
             return dp[idx][buy][k];
         }
         
-        int res = 0;
+        if(idx>=prices.length || k==0){
+            return 0;
+        }
+        
+        int maxprofit = 0;
         if(buy==0){
             int buyToday = -prices[idx]+sol(prices,idx+1,1,k,dp);
             int notBuy = sol(prices,idx+1,0,k,dp);
-            
-            res = Math.max(buyToday,notBuy);
+            maxprofit = Math.max(buyToday,notBuy);
         }
         else{
             int sellToday = prices[idx]+sol(prices,idx+1,0,k-1,dp);
             int notSell = sol(prices,idx+1,1,k,dp);
-            
-            res = Math.max(sellToday,notSell);
+            maxprofit = Math.max(sellToday,notSell);
         }
         
-        return dp[idx][buy][k] = res;
+        return dp[idx][buy][k] = maxprofit;
     }
 }
