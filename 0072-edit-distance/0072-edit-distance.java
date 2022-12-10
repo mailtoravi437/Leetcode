49c0aaa6331f57f3b1c9186e269b36319661174e
mailtoravi437 <68932649+mailtoravi437@users.noreply.github.com>
2022-12-10 19:01:17 +0530

    Time: 7 ms (80.53%), Space: 45.7 MB (6.35%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.java
+++ 0072-edit-distance/0072-edit-distance.java
@@ -4,11 +4,14 @@ class Solution {
         int m = word2.length();
         
         Integer dp[][] = new Integer[n+2][m+2];
-        
         return sol(word1,n,word2,m,dp);
     }
     
     public int sol(String word1,int n,String word2,int m,Integer dp[][]){
+        if(dp[n][m]!=null){
+            return dp[n][m];
+        }
+        
         if(n==0){
             return m;
         }
@@ -17,19 +20,16 @@ class Solution {
             return n;
         }
         
-        if(dp[n][m]!=null){
-            return dp[n][m];
-        }
-        
         if(word1.charAt(n-1)==word2.charAt(m-1)){
             return dp[n][m] = sol(word1,n-1,word2,m-1,dp);
         }
         
-        int insert = sol(word1,n-1,word2,m,dp);
-        int delete = sol(word1,n,word2,m-1,dp);
-        int replace = sol(word1,n-1,word2,m-1,dp);
         
+            int insert = sol(word1,n-1,word2,m,dp);
+            int delete = sol(word1,n,word2,m-1,dp);
+            int replace = sol(word1,n-1,word2,m-1,dp);
+            
+            return dp[n][m] = 1+Math.min(insert,Math.min(delete,replace));
         
-        return dp[n][m] = 1+Math.min(insert,Math.min(delete,replace));
     }
 }
