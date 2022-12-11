78aa91e5ae80b614024fb19726ea8c1a03772901
mailtoravi437 <68932649+mailtoravi437@users.noreply.github.com>
2022-12-11 12:11:28 +0530

    Time: 1241 ms (38.79%), Space: 40.5 MB (92.78%) - LeetHub

--- 0132-palindrome-partitioning-ii/0132-palindrome-partitioning-ii.java
+++ 0132-palindrome-partitioning-ii/0132-palindrome-partitioning-ii.java
@@ -6,37 +6,36 @@ class Solution {
     }
     
     public int sol(String s,int idx,int n,Integer dp[]){
-        if(idx==n){
-            return 0;
-        }
+         if(idx==n){
+             return 0;
+         }
         
         if(dp[idx]!=null){
             return dp[idx];
         }
         
         int ans = Integer.MAX_VALUE;
-        for(int k = idx;k<n;k++){
+        for(int k=idx;k<n;k++){
             if(isPalindrome(s,idx,k)){
                 int next = sol(s,k+1,n,dp);
-                int ans2 = 1+next;
-                
+                int ans2 = next+1;
                 ans = Math.min(ans,ans2);
             }
         }
         
-        return dp[idx]=ans;
+        return dp[idx] = ans;
     }
     
-    public boolean isPalindrome(String s,int start,int end){
-        while(start<end){
-            if(s.charAt(start)!=s.charAt(end)){
+    public boolean isPalindrome(String s,int i,int j){
+        while(i<j){
+            if(s.charAt(i)!=s.charAt(j)){
                 return false;
             }
             
-            start++;
-            end--;
+            i++;
+            j--;
         }
         
         return true;
     }
-}
+};
