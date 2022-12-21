c8724d609fe73e220d1f60743ff87c4688cbd0ad
mailtoravi437 <68932649+mailtoravi437@users.noreply.github.com>
2022-12-22 00:46:55 +0530

    Time: 1 ms (74.43%), Space: 41.9 MB (63.75%) - LeetHub

--- 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.java
+++ 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.java
@@ -16,9 +16,10 @@ class Solution {
         for(int i=0;i<n;i++){
             fast = fast.next;
         }
-        if (fast == null) return head.next;
-
         
+        if(fast==null){
+            return head.next;
+        }
         while(fast.next!=null){
             slow = slow.next;
             fast = fast.next;
