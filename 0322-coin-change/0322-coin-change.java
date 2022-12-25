a337042c1d50e4abb6a112efa4ccfacdfcb16de1
mailtoravi437 <68932649+mailtoravi437@users.noreply.github.com>
2022-12-25 12:40:54 +0530

    Time: 18 ms (88.04%), Space: 42.1 MB (84.29%) - LeetHub

--- 0322-coin-change/0322-coin-change.java
+++ 0322-coin-change/0322-coin-change.java
@@ -2,8 +2,8 @@ class Solution {
     public int coinChange(int[] coins, int amount) {
         int dp[] = new int[amount+1];
         Arrays.fill(dp,Integer.MAX_VALUE);
-        
         dp[0] = 0;
+        
         for(int i=1;i<=amount;i++){
             for(int j=0;j<coins.length;j++){
                 if(i>=coins[j] && dp[i-coins[j]]!=Integer.MAX_VALUE){
@@ -13,5 +13,5 @@ class Solution {
         }
         
         return dp[amount]==Integer.MAX_VALUE?-1:dp[amount];
-    }
+     }
 }
