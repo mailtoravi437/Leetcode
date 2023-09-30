f839e7b96bbc77f0940195cfc96648647969e0ab
mailtoravi437 <68932649+mailtoravi437@users.noreply.github.com>
2023-09-30 20:07:36 +0530

    Time: 0 ms (100.00%), Space: 39.1 MB (46.70%) - LeetHub

---
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divamazon-divdiv-classcompanytagscontainer-tagoccurence4-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div96-unique-binary-search-trees/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divamazon-divdiv-classcompanytagscontainer-tagoccurence4-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div96-unique-binary-search-trees.java
@@ -0,0 +1,24 @@
+class Solution {
+    Map<Integer,Integer> hmap = new HashMap<>();
+    public int numTrees(int n) {
+        if(n==0 || n==1){
+            return 1;
+        }
+        
+        int sum = 0;
+        for(int i=1;i<=n;i++){
+            if(!hmap.containsKey(i-1)){
+                hmap.put(i-1,numTrees(i-1));
+            }
+            
+            if(!hmap.containsKey(n-i)){
+                hmap.put(n-i,numTrees(n-i));
+            }
+            
+            sum += hmap.get(i-1)*hmap.get(n-i);
+        }
+        
+        return sum;
+    
+    }
+}
